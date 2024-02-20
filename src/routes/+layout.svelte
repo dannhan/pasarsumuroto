7d3eccbbf3d049dcbe57fd4609e22694531b9271
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-02-20 20:22:12 +0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.pcss';
+	import '@fontsource-variable/plus-jakarta-sans';
+  import Header from "./Header.svelte";
+</script>
+
+<Header />
+<slot />
+
+<style>
+	:global(body) {
+		font-family: 'Plus Jakarta Sans Variable', sans-serif;
+	}
+</style>
---
+++ src/routes/Products.svelte
@@ -0,0 +1,46 @@
+<section class="mx-auto grid max-w-screen-2xl py-12 text-center">
+	<h1 class="text-3xl font-extrabold">Produk</h1>
+	<h1 class="text-3xl font-extrabold">Pasar Sumuroto</h1>
+
+	<div class="grid grid-cols-4 gap-4">
+		<div class="flex flex-col items-center">
+			<img alt="" src="/assets/sembako@2x.png" class="h-[250px] w-[295px]" />
+			<h1 class="text-2xl font-extrabold">Sembako</h1>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img alt="" src="/assets/rectangle-15@2x.png" class="h-[250px] w-[295px]" />
+			<h1 class="text-2xl font-extrabold">Sayur Mayur</h1>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img alt="" src="/assets/rectangle-22@2x.png" class="h-[250px] w-[295px]" />
+			<h1 class="text-2xl font-extrabold">Daging</h1>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img alt="" src="/assets/rectangle-23@2x.png" class="h-[250px] w-[295px]" />
+			<h1 class="text-2xl font-extrabold">Telur</h1>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img loading="eager" alt="" src="/assets/rectangle-21@2x.png" class="h-[250px] w-[295px]" />
+			<h1 class="text-2xl font-extrabold">Aneka Jajanan</h1>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img alt="" src="/assets/rectangle-21-1@2x.png" class="h-[250px] w-[295px]" />
+			<h1 class="text-2xl font-extrabold">Makanan Ringan</h1>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img alt="" src="/assets/rectangle-21-2@2x.png" class="h-[250px] w-[295px]" />
+			<h1 class="text-2xl font-extrabold">Pakaian</h1>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img alt="" src="/assets/rectangle-21-3@2x.png" class="h-[250px] w-[295px]" />
+			<h1 class="text-2xl font-extrabold">Kebutuhan Rumah</h1>
+		</div>
+	</div>
+</section>
